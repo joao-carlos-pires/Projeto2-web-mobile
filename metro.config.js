b2fe42cb241edd29d861d9aabe01cd4a492c24b7
joao-carlos-pires <joao.2019150970@unicap.br>
2025-11-19 20:56:07 -0300

    pequenas alterações

--- metro.config.js
+++ metro.config.js
@@ -1,20 +1,19 @@
-// metro.config.js
+
 const { getDefaultConfig } = require("expo/metro-config");
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-// Guardamos o resolver padrão do Expo
+
 const originalResolveRequest = config.resolver.resolveRequest;
 
-// Interceptamos apenas os módulos "ws" e "stream"
+
 config.resolver.resolveRequest = (context, moduleName, platform) => {
-  // Qualquer require("ws") ou require("stream") vira módulo vazio
+ 
   if (moduleName === "ws" || moduleName === "stream") {
     return { type: "empty" };
   }
 
-  // Usa o resolver padrão do Expo
   if (originalResolveRequest) {
     return originalResolveRequest(context, moduleName, platform);
   }
